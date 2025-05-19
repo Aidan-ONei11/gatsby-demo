a975b6307d1a6d02d00cee2b3b7fd121aa88bf24
Aidan <155825807+Aidan-ONei11@users.noreply.github.com>
2025-05-18 23:16:03 -0400

    Add hero image using StaticImage plugin, complete step 3 in tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this following the Gatsby tutorial</p>
+      <StaticImage
+        alt="The pyramids in Giza at night with the milky way in the background"
+        src="../images/homepage-hero-cat.jpg"/>
+        
     </Layout>
   )
 }
