7a353a4fa0206b746c202cf16bec67211bd8225e
Aidan <155825807+Aidan-ONei11@users.noreply.github.com>
2025-05-21 19:28:09 -0400

    Feat: Add query to list blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,35 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const BlogPage = () => {
+
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
             <p>Welcome to Planet E-Com: Tales from Beyond the Fold. Dispatches from the Galaxy of Online Selling.</p>
         </Layout>
     )
 }
+
+export const query = graphql`
+query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+  `
 
 export const Head = () => <Seo title="My Blog Posts"/>
 
